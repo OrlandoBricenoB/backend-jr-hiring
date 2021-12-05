eee08de8fc7a866e1104aa6a15b9c59a116a92d3
Jo-Sword <joswordd@gmail.com>
2021-12-05 18:37:19 -0400

    fix: querying MongoDB and handle exceptions.

--- test3.js
+++ test3.js
@@ -6,23 +6,50 @@ class Server {
     this.db = db
   }
 
+  _getCollection(collectionName, query) {
+    if (!this.db[collectionName]) return [new BadRequest(), null]
+    if (!query) return [new NotFound(), null]
+
+    const collection = this.db[collectionName]
+    if (!Array.isArray(collection)) return [new NotFound(), null]
+
+    return [null, collection]
+  }
+
+  _editNotFoundMsg(error, message) {
+    error.message = message
+    return error
+  }
+
   find(collectionName, query) {
-      if (!this.db[collectionName] || !query) return new BadRequest()
+    const [error, collection] = this._getCollection(collectionName, query)
+    if (error) return error
 
-      const collection = this.db[collectionName]
-      if (!Array.isArray(collection)) return new NotFound()
-
-      const queryDocument = collection.filter(sift(query))
-      if (!queryDocument.length) return new NotFound()
-      return queryDocument
+    const queryDocuments = collection.filter(sift(query))
+    if (!queryDocuments.length) return new NotFound()
+    return queryDocuments
   }
 
   findOne(collectionName, id) {
-    return {}
+    const query = id && { id: { $eq: id } }
+    const [error, collection] = this._getCollection(collectionName, query)
+    if (error) return this._editNotFoundMsg(error, 'No data found with the id equal as "null".') 
+
+    const queryDocuments = collection.filter(sift(query))
+    const [ firstDocument ] = queryDocuments
+    return firstDocument
   }
 
   updateOne(collectionName, id, properties) {
-    return {}
+    const query = id && { id: { $eq: id } }
+    const [error, collection] = this._getCollection(collectionName, query, properties)
+    if (error) return this._editNotFoundMsg(error, 'No data for update found with the id equal as "null".') 
+
+    const queryDocuments = collection.filter(sift(query))
+    const [ firstDocument ] = queryDocuments
+
+    const updatedDocument = Object.assign(firstDocument, properties)
+    return updatedDocument
   }
 }
 
